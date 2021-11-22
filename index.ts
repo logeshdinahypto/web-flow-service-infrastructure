6a959bc7477e5d5d936f89ef11c3c9832c365730
logeshdinahypto <logeshdina@hypto.in>
2021-11-22 18:27:22 +0530

    Hypto - Initial commit

---
+++ index.ts
@@ -0,0 +1,82 @@
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecr = require('@aws-cdk/aws-ecr');
+import ecs = require('@aws-cdk/aws-ecs');
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
+import cdk = require('@aws-cdk/core');
+import {ApplicationProtocol, ApplicationProtocolVersion} from "@aws-cdk/aws-elasticloadbalancingv2";
+import {HostedZone} from "@aws-cdk/aws-route53";
+import {RemovalPolicy} from "@aws-cdk/core";
+import {Environment} from "@aws-cdk/core/lib/environment";
+import {DnsValidatedCertificate} from "@aws-cdk/aws-certificatemanager";
+
+class WebFlowServiceRepository extends cdk.Stack {
+  public readonly repository: ecr.IRepository;
+
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    // Create ECR repository
+    this.repository = new ecr.Repository(this, 'web-flow-service', {
+      repositoryName: 'web-flow-service',
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+  }
+}
+
+interface WebFlowServiceRepositoryProps extends cdk.StackProps {
+  readonly repository: ecr.IRepository;
+  readonly env?: Environment;
+}
+
+class WebFlowServiceFargate extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props: WebFlowServiceRepositoryProps) {
+    super(scope, id, props);
+
+    const zoneName = 'hypto.co.in';
+    const domainName = `web-flow.${zoneName}`;
+    const domainZone = HostedZone.fromLookup(this, 'StagingZone', { domainName: zoneName });
+
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.Vpc(this, 'WebFlowServiceVpc', { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, 'WebFlowServiceCluster', { vpc });
+
+    const certificate = new DnsValidatedCertificate(this, 'Certificate', {
+      domainName,
+      hostedZone: domainZone
+    });
+
+    // Instantiate Fargate Service with an application load balancer
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "WebFlowService", {
+      cluster,
+      protocol: ApplicationProtocol.HTTPS,
+      listenerPort: 50051,
+      domainName,
+      domainZone,
+      certificate,
+      targetProtocol: ApplicationProtocol.HTTP,
+      protocolVersion: ApplicationProtocolVersion.GRPC,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(props.repository),
+        containerPort: 50051
+      },
+    });
+  }
+}
+
+const app = new cdk.App();
+
+const repositoryStack = new WebFlowServiceRepository(app, 'repo', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
+new WebFlowServiceFargate(app, 'service', {
+  repository: repositoryStack.repository,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
+
+app.synth();
